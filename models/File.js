a497f866ff6b6976b4f39c76c61b9bb7af712bd1
shubham30102002 <shubhamrg30@gmail.com>
2024-03-23 16:30:49 +0530

    mailing func

--- models/File.js
+++ models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -15,6 +17,38 @@ const fileSchema = new mongoose.Schema({
         type:String,
     }
 });
+
+//post middleware
+fileSchema.post("save" , async function(doc) {
+    try{
+        //database entry entire object is store in this doc
+        console.log("Doc -> ",doc);
+
+        //transporter
+        //shift this configuration under config folder
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        })
+
+        //send mail
+        let info = await transporter.sendMail({
+            from: `FileUpload by Shubham`,
+            to: doc.email,
+            subject: "New File uploaded on cloudinary",
+            html: `<h2>Thank You jee</h2> <p>File Uploaded</p> View here : <a href=${doc.imageUrl}>${doc.imageUrl}</a>`
+        })
+        console.log(info);
+
+    }catch(error){
+        console.log(error);
+     
+
+    }
+})
 
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
